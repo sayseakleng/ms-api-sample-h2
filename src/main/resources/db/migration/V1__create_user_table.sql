e8d1c0b517ea491c8b798123dd48f66ceda30225
Seakleng Say <seakleng.say@wingmoney.com>
2025-09-10 17:22:41 +0700

    Format setting

--- src/main/resources/db/migration/V1__create_user_table.sql
+++ src/main/resources/db/migration/V1__create_user_table.sql
@@ -2,13 +2,14 @@
 CREATE SEQUENCE users_seq START WITH 1 INCREMENT BY 50;
 
 -- Create table
-CREATE TABLE users (
-    user_id BIGINT PRIMARY KEY,
-    phone VARCHAR(20) NOT NULL UNIQUE,
+CREATE TABLE users
+(
+    user_id    BIGINT PRIMARY KEY,
+    phone      VARCHAR(20) NOT NULL UNIQUE,
     first_name VARCHAR(50) NOT NULL,
-    last_name VARCHAR(50) NOT NULL,
-    gender VARCHAR(1) NOT NULL
+    last_name  VARCHAR(50) NOT NULL,
+    gender     VARCHAR(1)  NOT NULL
 );
 
-INSERT INTO users (user_id, phone, first_name, last_name, gender) VALUES
-(1, '016666666', 'First', 'Last', 'M');
+INSERT INTO users (user_id, phone, first_name, last_name, gender)
+VALUES (1, '016666666', 'First', 'Last', 'M');
